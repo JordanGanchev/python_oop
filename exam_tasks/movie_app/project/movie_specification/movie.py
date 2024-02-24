a78f3e465de4f2d51c2dc6d3a198ef4347a6d9ea
Jordan Ganchev <jordan.ganchev.1434@gmail.com>
2024-02-24 21:15:36 +0200

    exercise

---
+++ exam_tasks/movie_app/project/movie_specification/movie.py
@@ -0,0 +1,59 @@
+from abc import ABC, abstractmethod
+
+from project.user import User
+from project.utils.validators import validate_non_empty_string, validate_greater_then_value
+
+
+class Movie(ABC):
+    MIN_YEAR = 1888
+
+    def __init__(self, title: str, year: int, owner: object, age_restriction: int):
+        self.title = title
+        self.year = year
+        self.owner = owner
+        self.age_restriction = age_restriction
+        self.likes = 0
+
+    @property
+    def title(self):
+        return self.__title
+
+    @title.setter
+    def title(self, value):
+        self.__validate_title(value)
+        self.__title = value
+
+    @property
+    def year(self):
+        return self.__year
+
+    @year.setter
+    def year(self, value):
+        self.__validate_year(value)
+        self.__year = value
+
+    @property
+    def owner(self):
+        return self.__owner
+
+    @owner.setter
+    def owner(self, value):
+        self.validate_owner(value)
+        self.__owner = value
+
+    @abstractmethod
+    def details(self):
+        pass
+
+    @staticmethod
+    def __validate_title(title):
+        validate_non_empty_string(title, "The title cannot be empty string!")
+
+    @classmethod
+    def __validate_year(cls, year):
+        validate_greater_then_value(year, cls.MIN_YEAR, f"Movies weren't made before {cls.MIN_YEAR}!")
+
+    @staticmethod
+    def validate_owner(owner):
+        if owner is not User:
+            raise ValueError("The owner must be an object of type User!")
