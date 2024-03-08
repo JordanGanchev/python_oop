02bd3ed53f808745e337c091595631da5e91c653
Jordan Ganchev <jordan.ganchev.1434@gmail.com>
2024-03-08 08:00:43 +0200

    exercise

--- exam_tasks_24/test_trains/tests/test_train.py
+++ exam_tasks_24/test_trains/tests/test_train.py
@@ -22,11 +22,29 @@ class TestTrain(unittest.TestCase):
         self.assertEqual([], self.train.passengers)
 
     def test_add_passenger(self):
-        self.train.passengers.append('lubo', 'emo', 'boris')
-        self.train.add('lubo')
-        if len(self.train.passengers) == 2:
-            self.assertEqual("Train is full", )
+        self.train.passengers.append('boris')
+        self.train.capacity = 1
+        with self.assertRaises(ValueError) as content:
+            self.train.add('monk')
+        self.assertEqual("Train is full", str(content.exception))
 
+    def test_add_name_if_name_in_passenger(self):
+        self.train.passengers.append('boris')
+        with self.assertRaises(ValueError) as content:
+            self.train.add('boris')
+        self.assertEqual(Train.PASSENGER_EXISTS.format('boris'), str(content.exception))
 
+    def test_add_name_if_not_in_passenger(self):
+        self.train.passengers.append('boris')
+        self.train.add('monk')
+        self.assertEqual(self.train.passengers, ['boris', 'monk'])
 
+    def test_remove_if_not_in_passenger(self):
+        with self.assertRaises(ValueError) as content:
+            self.train.remove('monk')
+        self.assertEqual(Train.PASSENGER_NOT_FOUND.format('monk'), str(content.exception))
 
+    def test_remove_if_have_passenger(self):
+        self.train.passengers.append('boris')
+        self.train.remove('boris')
+        self.assertEqual(self.train.passengers, [])
