91d46d9a96e8dd044700b4507a0c0c28c887534a
Jordan Ganchev <jordan.ganchev.1434@gmail.com>
2024-03-11 21:42:45 +0200

    exercise

--- exam_tasks_24/Summitville_last_exam/project/summit_quest_manager_app.py
+++ exam_tasks_24/Summitville_last_exam/project/summit_quest_manager_app.py
@@ -1,42 +1,98 @@
 from typing import List
 
 from project.climbers.arctic_climber import ArcticClimber
+from project.climbers.base_climber import BaseClimber
 from project.climbers.summit_climber import SummitClimber
 from project.peaks.arctic_peak import ArcticPeak
+from project.peaks.base_peak import BasePeak
 from project.peaks.summit_peak import SummitPeak
 
 
-
 class SummitQuestManagerApp:
-    VALID_PEAK = {"ArcticPeak": ArcticPeak, "SummitPeak": SummitPeak}
-    VALID_CLIMBER = {"ArcticClimber": ArcticClimber, "SummitClimber": SummitClimber}
+    TYPE_CLIMBER = {'ArcticClimber': ArcticClimber, 'SummitClimber': SummitClimber}
+    TYPE_PEAK = {'ArcticPeak': ArcticPeak, 'SummitPeak': SummitPeak}
 
     def __init__(self):
-        self.climbers: List = []
-        self.peaks: list = []
+        self.climbers: List[BaseClimber] = []
+        self.peaks: List[BasePeak] = []
 
     def register_climber(self, climber_type: str, climber_name: str):
-        if climber_type not in self.VALID_CLIMBER:
+        if climber_type not in self.TYPE_CLIMBER:
             return f"{climber_type} doesn't exist in our register."
-        name = next((n for n in self.climbers if n.name == climber_name), None)
-        if name is not None:
+        climber = self._get_climber(climber_name)
+        if climber is not None:
             return f"{climber_name} has been already registered."
-        new_climber = self.VALID_CLIMBER[climber_type](climber_name)
+        new_climber = self.TYPE_CLIMBER[climber_type](climber_name)
         self.climbers.append(new_climber)
         return f"{climber_name} is successfully registered as a {climber_type}."
 
     def peak_wish_list(self, peak_type: str, peak_name: str, peak_elevation: int):
-        if peak_type not in self.VALID_PEAK:
+        if peak_type not in self.TYPE_PEAK:
             return f"{peak_type} is an unknown type of peak."
-        new_peak = self.VALID_PEAK[peak_type](peak_name, peak_elevation)
+        new_peak = self.TYPE_PEAK[peak_type](peak_name, peak_elevation)
         self.peaks.append(new_peak)
-        return f'{peak_name} is successfully added to the wish list as a {peak_type}.'
+        return f"{peak_name} is successfully added to the wish list as a {peak_type}."
+
+    # def check_gear(self, climber_name: str, peak_name: str, gear: List[str]):
+    #     pass
+    #
+    # def perform_climbing(self, climber_name: str, peak_name: str):
+    #     pass
+    #
+    # def get_statistics(self):
+    #     pass
 
     def check_gear(self, climber_name: str, peak_name: str, gear: List[str]):
-        pass
+        climber = self._get_climber(climber_name)
+        peak = self._get_peak(peak_name)
+
+        required_gear = set(peak.get_recommended_gear())
+        missing_gear = required_gear - set(gear)
+
+        if missing_gear:
+            climber.is_prepared = False
+            sorted_missing_gear = sorted(missing_gear)
+            return f"{climber_name} is not prepared to climb {peak_name}. Missing gear: {', '.join(sorted_missing_gear)}."
+        else:
+            return f"{climber_name} is prepared to climb {peak_name}."
 
     def perform_climbing(self, climber_name: str, peak_name: str):
-        pass
+        climber = self._get_climber(climber_name)
+        peak = self._get_peak(peak_name)
+
+        if not climber:
+            return f"Climber {climber_name} is not registered yet."
+
+        if not peak:
+            return f"Peak {peak_name} is not part of the wish list."
+
+        if climber.can_climb() and climber.is_prepared:
+            climber.climb(peak)
+            return f"{climber_name} conquered {peak_name} whose difficulty level is {peak.difficulty_level}."
+        elif not climber.is_prepared:
+            return f"{climber_name} will need to be better prepared next time."
+        else:
+            climber.rest()
+            return f"{climber_name} needs more strength to climb {peak_name} and is therefore taking some rest."
 
     def get_statistics(self):
-        pass
+        sorted_climbers = sorted([climber for climber in self.climbers if climber.conquered_peaks],
+                                 key=lambda climber: (-len(climber.conquered_peaks), climber.name))
+
+        result = [
+            f"Total climbed peaks: {len(self.peaks)}",
+            "**Climber's statistics:**"
+        ]
+
+        climber_statistics = "\n".join(str(c) for c in sorted_climbers)
+        result.append(climber_statistics)
+
+        return '\n'.join(result)
+
+    def _get_climber(self, climber_name):
+        climber = [c for c in self.climbers if c.name == climber_name]
+        return climber[0] if climber else None
+
+    def _get_peak(self, peak_name):
+        peak = [p for p in self.peaks if p.name == peak_name]
+        return peak[0] if peak else None
