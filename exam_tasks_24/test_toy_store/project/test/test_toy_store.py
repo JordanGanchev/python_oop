73bc8aab47f6814cb23f0bf660a07096c98fb898
Jordan Ganchev <jordan.ganchev.1434@gmail.com>
2024-03-25 19:13:29 +0200

    exercise

--- exam_tasks_24/test_toy_store/project/test/test_toy_store.py
+++ exam_tasks_24/test_toy_store/project/test/test_toy_store.py
@@ -1,1 +1,18 @@
+import unittest
 from project.toy_store import ToyStore
+
+
+class TestToyStore(unittest.TestCase):
+    def setUp(self) -> None:
+        self.toy = ToyStore()
+
+    def test_init_dictionaries(self):
+        self.assertEqual({
+            "A": None,
+            "B": None,
+            "C": None,
+            "D": None,
+            "E": None,
+            "F": None,
+            "G": None,
+        }, self.toy.toy_shelf)
