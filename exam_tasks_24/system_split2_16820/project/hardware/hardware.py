0beff0d2b012ecb343b681e275f97972d28ec42e
Jordan Ganchev <jordan.ganchev.1434@gmail.com>
2024-04-04 06:33:27 +0300

    exercise

--- exam_tasks_24/system_split2_16820/project/hardware/hardware.py
+++ exam_tasks_24/system_split2_16820/project/hardware/hardware.py
@@ -1,31 +1,3 @@
-# from typing import List
-#
-# from project.software.software import Software
-#
-#
-# class Hardware:
-#     def __init__(self, name: str, hardware_type: str, capacity: int, memory: int):
-#         self.name = name
-#         self.hardware_type = hardware_type
-#         self.capacity = capacity
-#         self.memory = memory
-#         self.software_components: List = []
-#
-#     def install(self, software: Software):
-#         soft_comp = [soft for soft in self.software_components if soft.software == software]
-#         if soft_comp:
-#             capacity = soft_comp[2]
-#             memory = soft_comp[3]
-#             if self.capacity > capacity and self.memory > memory:
-#                 self.software_components.append(software)
-#             raise Exception("Software cannot be installed")
-#
-#     def uninstall(self, software: Software):
-#         soft_comp = [soft for soft in self.software_components if soft.software == software]
-#         if soft_comp:
-#             self.software_components.remove(software)
-
-
 from typing import List
 
 from project.software.software import Software
@@ -37,11 +9,11 @@ class Hardware:
         self.hardware_type = hardware_type
         self.capacity = capacity
         self.memory = memory
-        self.software_components: List = []
+        self.software_components: List[Software] = []
 
     @property
     def available_memory(self):
-        return self.memory - sum([s.memory_consumpion for s in self.software_components])
+        return self.memory - sum([s.memory_consumption for s in self.software_components])
 
     @property
     def available_capacity(self):
--- exam_tasks_24/system_split2_16820/project/system.py
+++ exam_tasks_24/system_split2_16820/project/system.py
@@ -5,26 +5,24 @@ from project.software.light_software import LightSoftware
 
 
 class System:
-    hardware = []
-    software = []
+    _hardware = []
+    _software = []
 
     @staticmethod
     def register_power_hardware(name: str, capacity: int, memory: int):
-        heavy = HeavyHardware(name, capacity, memory)
-        System.hardware.append(heavy)
+        System._hardware.append(PowerHardware(name, capacity, memory))
 
     @staticmethod
     def register_heavy_hardware(name: str, capacity: int, memory: int):
-        pover = PowerHardware(name, capacity, memory)
-        System.software.append(pover)
+        System._hardware.append(HeavyHardware(name, capacity, memory))
 
     @staticmethod
     def register_express_software(hardware_name: str, name: str, capacity_consumption: int, memory_consumption: int):
         try:
-            hardware = [h for h in System.hardware if h.name == hardware_name][0]
+            hardware = [h for h in System._hardware if h.name == hardware_name][0]
             software = ExpressSoftware(name, capacity_consumption, memory_consumption)
             hardware.install(software)
-            System.software.append(software)
+            System._software.append(software)
         except IndexError:
             return "Hardware does not exist"
         except Exception as ex:
@@ -33,10 +31,10 @@ class System:
     @staticmethod
     def register_light_software(hardware_name: str, name: str, capacity_consumption: int, memory_consumption: int):
         try:
-            hardware = [h for h in System.hardware if h.name == hardware_name][0]
+            hardware = [h for h in System._hardware if h.name == hardware_name][0]
             software = LightSoftware(name, capacity_consumption, memory_consumption)
             hardware.install(software)
-            System.software.append(software)
+            System._software.append(software)
         except IndexError:
             return "Hardware does not exist"
         except Exception as ex:
@@ -44,12 +42,12 @@ class System:
 
     @staticmethod
     def release_software_component(hardware_name: str, software_name: str):
-        hardware = next((h for h in System.hardware if h.name == hardware_name), None)
-        software = next((s for s in System.hardware if s.name == software_name), None)
-        if hardware is None or software is None:
+        try:
+            hardware = [h for h in System._hardware if h.name == hardware_name][0]
+            software = [s for s in System._software if s.name == software_name][0]
+            hardware.uninstall(software)
+        except IndexError:
             return "Some of the components do not exist"
-        hardware.uninstall(software)
-        System.software.remove(software)
 
 
     @staticmethod
@@ -59,7 +57,8 @@ class System:
                f"Hardware Components: {len(System.hardware)}\n" \
                f"Software Components: {len(System.software)}\n" \
                f"Total Operational Memory: {total memory consumption for all registered software components} / {total memory for all registered hardware components}\n" \
-               f"Total Capacity Taken: {total capacity consumption for all registered software components} / {total capacity of all registered hardware components}"    @staticmethod
+               f"Total Capacity Taken: {total capacity consumption for all registered software components} / {total capacity of all registered hardware components}"
+    @staticmethod
     def system_split():
         pass
 
