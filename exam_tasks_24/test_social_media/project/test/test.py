44f2519f440a0f130ca6a3b94f204d43605d9ad1
Jordan Ganchev <jordan.ganchev.1434@gmail.com>
2024-04-12 20:58:50 +0300

    exercise

--- exam_tasks_24/test_social_media/project/test/test.py
+++ exam_tasks_24/test_social_media/project/test/test.py
@@ -10,5 +10,10 @@ class TestSocialMedia(unittest.TestCase):
     def test_init_method(self):
         self.assertEqual('dan', self.social._username)
         self.assertEqual('Instagram', self.social._platform)
+        self.assertEqual(None, self.social._validate_and_set_platform('Instagram'))
+        self.assertEqual(100, self.social.followers)
+        self.assertEqual('people', self.social._content_type)
+        self.assertEqual([], self.social._posts)
 
-
+    def test_platform_if_not_allowed_platforms(self)
+        pass:
